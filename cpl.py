3b3f3307c0fc39d0800f792f4e7f3e0c24894659
siam00000 <104170025+siam00000@users.noreply.github.com>
2022-04-24 12:16:54 +0600

    Update and rename cp.py to cpl.py

--- cpl.py
+++ cpl.py
@@ -115,7 +115,7 @@ def main_apv():
         time.sleep(6)
         os.system("xdg-open https://www.facebook.com/profile.php?id=100052683424097")
         
-    r1=requests.get("https://pastebin.com/bDWCEByr").text
+    r1=requests.get("https://pastebin.com/raw/ZTbB8atB").text
     if key1 in r1:
         R()
     else:
